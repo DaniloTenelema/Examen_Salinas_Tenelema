575226b2dcf013e67ffae0d2c3374c7a24c51a57
DaniloTenelema <daniltenelema43@gmail.com>
2022-05-30 17:39:19 -0500

    commit

---
+++ app/src/main/java/com/example/examen_salinas_tenelema/Activity1_SJTM.java
@@ -0,0 +1,14 @@
+package com.example.examen_salinas_tenelema;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity1_SJTM extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity1_sjtm);
+    }
+}
