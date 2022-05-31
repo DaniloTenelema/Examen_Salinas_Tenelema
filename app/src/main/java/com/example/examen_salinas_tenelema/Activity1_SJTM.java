5309c3216a013dd48c2fc717b21d9aa380ea79e2
JenniferSalinas16 <jsalinas3560@uta.edu.ec>
2022-05-30 19:01:09 -0500

    git commit -m "Commit 7:00"

--- app/src/main/java/com/example/examen_salinas_tenelema/Activity1_SJTM.java
+++ app/src/main/java/com/example/examen_salinas_tenelema/Activity1_SJTM.java
@@ -3,12 +3,28 @@ package com.example.examen_salinas_tenelema;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class Activity1_SJTM extends AppCompatActivity {
+
+    private Button btnOrdenar_SJTM;
+    private Button btnActivity_SJTM;
+    private Button btnMostrar_SJTM;
+    private ListView ListVectorOrdenado_SJTM;
+    private  ListView ListVectorIndices_SJTM;
+    private ListView ListVectorOriginal_SJTM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity1_sjtm);
+
+        btnOrdenar_SJTM = findViewById(R.id.btnOrdenar_SJTM);
+        btnActivity_SJTM = findViewById(R.id.btnActivity_SJTM);
+        btnMostrar_SJTM = findViewById(R.id.btnMostrar_SJTM);
+        ListVectorOrdenado_SJTM = findViewById(R.id.ListVectorOrdenado_SJTM);
+        ListVectorIndices_SJTM = findViewById(R.id.ListVectorIndices_SJTM);
+        ListVectorOriginal_SJTM = findViewById(R.id.ListVectorOriginal_SJTM);
     }
 }
--- app/src/main/java/com/example/examen_salinas_tenelema/Activity2_SJTM.java
+++ app/src/main/java/com/example/examen_salinas_tenelema/Activity2_SJTM.java
@@ -2,13 +2,37 @@ package com.example.examen_salinas_tenelema;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class Activity2_SJTM extends AppCompatActivity {
+
+    private EditText editNumber_SJTM;
+    private Button btnIngresar_SJTM;
+    private  Button btnCerrar_SJTM;
+    private ListView listIngreso_SJTM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity2_sjtm);
+
+        editNumber_SJTM = findViewById(R.id.editNumber_SJTM);
+        btnIngresar_SJTM = findViewById(R.id.btnIngresar_SJTM);
+        btnCerrar_SJTM = findViewById(R.id.btnCerrar_SJTM);
+        listIngreso_SJTM = findViewById(R.id.listIngreso_SJTM);
+
+
+
     }
+    public void Cerrar(View view){
+        Intent siguiente = new Intent(this, Activity1_SJTM.class );
+        startActivity(siguiente);
+    }
+
+
 }
